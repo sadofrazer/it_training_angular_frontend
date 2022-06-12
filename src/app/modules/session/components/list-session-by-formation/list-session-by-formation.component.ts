64fb72c3384d7d87aca0d951258dcdfd4f15defb
sadofrazer <sadofrazer@yahoo.fr>
2022-06-12 14:57:33 +0200

    fix errors

---
+++ src/app/modules/session/components/list-session-by-formation/list-session-by-formation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-session-by-formation',
+  templateUrl: './list-session-by-formation.component.html',
+  styleUrls: ['./list-session-by-formation.component.scss']
+})
+export class ListSessionByFormationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
